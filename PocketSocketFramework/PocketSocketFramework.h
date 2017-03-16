8e691d606932a860bd2b2599aace07c4e6e90225
Chris McMeeking <chris.mcmeeking@deque.com>
2017-03-16 14:35:54 -0400

    Added framework target.

---
+++ PocketSocketFramework/PocketSocketFramework.h
@@ -0,0 +1,22 @@
+//
+//  PocketSocketFramework.h
+//  PocketSocketFramework
+//
+//  Created by Chris McMeeking on 3/16/17.
+//  Copyright © 2017 Zwopple Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSWebSocket.h"
+#import "PSWebSocketServer.h"
+
+//! Project version number for PocketSocketFramework.
+FOUNDATION_EXPORT double PocketSocketFrameworkVersionNumber;
+
+//! Project version string for PocketSocketFramework.
+FOUNDATION_EXPORT const unsigned char PocketSocketFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSocketFramework/PublicHeader.h>
+
+
